6fbb365b99f70e7d046661894806fc6cd75500e2
XxShenglixX <c_shengli@hotmail.com>
2014-06-01 21:41:39 +0800

    Added test for both first name and last name together. Complete Test Person Info

--- build/test/preprocess/files/test_PersonInfo.c
+++ build/test/preprocess/files/test_PersonInfo.c
@@ -20,6 +20,10 @@ void tearDown(void)
 
 
 
+
+
+
+
 void test_setAge_given_negative1_should_return0()
 
 {
@@ -32,25 +36,7 @@ void test_setAge_given_negative1_should_return0()
 
  indicator = setAge(&test,test.age);
 
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)19, UNITY_DISPLAY_STYLE_INT);
-
-}
-
-
-
-void test_setAge_given_199_should_return0()
-
-{
-
- PersonInfo test = {.age = 199};
-
-
-
- int indicator;
-
- indicator = setAge(&test,test.age);
-
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)28, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)21, UNITY_DISPLAY_STYLE_INT);
 
 }
 
@@ -68,7 +54,7 @@ void test_setAge_given_131_should_return0()
 
  indicator = setAge(&test,test.age);
 
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)37, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)30, UNITY_DISPLAY_STYLE_INT);
 
 }
 
@@ -86,7 +72,7 @@ void test_setAge_given_130_should_return1()
 
  indicator = setAge(&test,test.age);
 
- UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)46, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)39, UNITY_DISPLAY_STYLE_INT);
 
 }
 
@@ -104,7 +90,7 @@ void test_setAge_given_0_should_return1()
 
  indicator = setAge(&test,test.age);
 
- UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)55, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)48, UNITY_DISPLAY_STYLE_INT);
 
 }
 
@@ -122,9 +108,11 @@ void test_setAge_given_99_should_return1()
 
  indicator = setAge(&test,test.age);
 
- UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)64, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)57, UNITY_DISPLAY_STYLE_INT);
 
 }
+
+
 
 
 
@@ -140,7 +128,7 @@ void test_setHeight_given_negative_0point01should_return0()
 
  indicator = setHeight(&test,test.height);
 
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)73, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)67, UNITY_DISPLAY_STYLE_INT);
 
 }
 
@@ -158,29 +146,9 @@ void test_setHeight_given_3point001_should_return0()
 
  indicator = setHeight(&test,test.height);
 
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)82, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)76, UNITY_DISPLAY_STYLE_INT);
 
 }
-
-
-
-void test_setHeight_given_100_should_return0()
-
-{
-
- PersonInfo test = {.height = 100};
-
-
-
- int indicator;
-
- indicator = setHeight(&test,test.height);
-
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)91, UNITY_DISPLAY_STYLE_INT);
-
-}
-
-
 
 
 
@@ -196,7 +164,25 @@ void test_setHeight_given_0_should_return1()
 
  indicator = setHeight(&test,test.height);
 
- UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)101, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)85, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setHeight_given_3_should_return1()
+
+{
+
+ PersonInfo test = {.height = 3};
+
+
+
+ int indicator;
+
+ indicator = setHeight(&test,test.height);
+
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)94, UNITY_DISPLAY_STYLE_INT);
 
 }
 
@@ -214,27 +200,11 @@ void test_setHeight_given_1point7_should_return1()
 
  indicator = setHeight(&test,test.height);
 
- UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)110, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)103, UNITY_DISPLAY_STYLE_INT);
 
 }
 
 
-
-void test_setHeight_given_3m_should_return1()
-
-{
-
- PersonInfo test = {.height = 3};
-
-
-
- int indicator;
-
- indicator = setHeight(&test,test.height);
-
- UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)119, UNITY_DISPLAY_STYLE_INT);
-
-}
 
 
 
@@ -250,25 +220,7 @@ void test_setTelephone_given_00012345_should_return0()
 
  indicator = setTelephone(&test,test.telephone);
 
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)128, UNITY_DISPLAY_STYLE_INT);
-
-}
-
-
-
-void test_setTelephone_given_123456789_should_return0()
-
-{
-
- PersonInfo test = {.telephone = 123456789};
-
-
-
- int indicator;
-
- indicator = setTelephone(&test,test.telephone);
-
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)137, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)113, UNITY_DISPLAY_STYLE_INT);
 
 }
 
@@ -286,13 +238,13 @@ void test_setTelephone_given_100000000_should_return0()
 
  indicator = setTelephone(&test,test.telephone);
 
- UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)146, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)122, UNITY_DISPLAY_STYLE_INT);
 
 }
 
 
 
-void test_setTelephone_given10023456_should_return1()
+void test_setTelephone_given_10023456_should_return1()
 
 {
 
@@ -304,6 +256,464 @@ void test_setTelephone_given10023456_should_return1()
 
  indicator = setTelephone(&test,test.telephone);
 
- UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)155, UNITY_DISPLAY_STYLE_INT);
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)131, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+
+
+
+
+void test_setFirstName_given_emptystring()
+
+{
+
+ PersonInfo test = {.firstName = ((void *)0)};
+
+
+
+ int indicator;
+
+ indicator = setFirstName(&test,((void *)0));
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)142, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_given_B3njamin()
+
+{
+
+ PersonInfo test = {.firstName = "B3njamin"};
+
+
+
+ int indicator;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)151, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_given_blank_Benjamin()
+
+{
+
+ PersonInfo test = {.firstName = " Benjamin"};
+
+
+
+ int indicator;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)160, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_given_alliance_Benjamin()
+
+{
+
+ PersonInfo test = {.firstName = "@Benjamin"};
+
+
+
+ int indicator;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)169, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_given_Aaron_Anthony_Benjamin_Bernard_27characters_name()
+
+{
+
+ PersonInfo test = {.firstName = "AaronAnthonyBenjaminBernard"};
+
+
+
+ int indicator;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)178, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_given_Benjamin()
+
+{
+
+ PersonInfo test = {.firstName = "Benjamin"};
+
+
+
+ int indicator;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)187, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_given_Benjamin_blank_Ooi()
+
+{
+
+ PersonInfo test = {.firstName = "Benjamin Ooi"};
+
+
+
+ int indicator;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)196, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+
+
+void test_setLastName_given_emptystring()
+
+{
+
+ PersonInfo test = {.lastName = ((void *)0)};
+
+
+
+ int indicator;
+
+ indicator = setLastName(&test,((void *)0));
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)206, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setLastName_given_B3ng()
+
+{
+
+ PersonInfo test = {.lastName = "B3njamin"};
+
+
+
+ int indicator;
+
+ indicator = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)215, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setLastName_given_blank_Huat()
+
+{
+
+ PersonInfo test = {.lastName = " Huat"};
+
+
+
+ int indicator;
+
+ indicator = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)224, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setLastName_given_slash_Hui()
+
+{
+
+ PersonInfo test = {.lastName = "/Hui"};
+
+
+
+ int indicator;
+
+ indicator = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)233, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setLastName_given_abcdefghijklmnopqrstuvwxyz_26characters_name()
+
+{
+
+ PersonInfo test = {.lastName = "abcdefghijklmnopqrstuvwxyz"};
+
+
+
+ int indicator;
+
+ indicator = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)242, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setLastName_given_Beng()
+
+{
+
+ PersonInfo test = {.lastName = "Beng"};
+
+
+
+ int indicator;
+
+ indicator = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)251, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setLastName_given_Ah_blank_Huat()
+
+{
+
+ PersonInfo test = {.lastName = "Ah Huat"};
+
+
+
+ int indicator;
+
+ indicator = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)((indicator)), (((void *)0)), (_U_UINT)260, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+void test_setFirstName_setLastName_given_Tan__Ah_B3ng()
+
+{
+
+ PersonInfo test = {"Tan","Ah B3ng"};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ indicator_lastName = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)274, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_setLastName_given_Tan__Ah_Be_symbol_g()
+
+{
+
+ PersonInfo test = {"Tan","Ah Be|\|g"};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ indicator_lastName = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)284, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_setLastName_given_Tan__AhBang_AhBeng_AhBing_Ahh_21_characters_name()
+
+{
+
+ PersonInfo test = {"Tan","AhBangAhBengAhBingAhh"};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ indicator_lastName = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)294, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+
+
+void test_setFirstName_setLastName_given_Tan__null()
+
+{
+
+ PersonInfo test = {"Tan",((void *)0)};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ indicator_lastName = setLastName(&test,((void *)0));
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)305, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void test_setFirstName_setLastName_given_T_alliance_n__Ah_Beng()
+
+{
+
+ PersonInfo test = {"T@n","Ah Beng"};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ indicator_lastName = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)321, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_setLastName_given_T8n__Ah_Beng()
+
+{
+
+ PersonInfo test = {"T8n","Ah Beng"};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ indicator_lastName = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)331, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_setLastName_given_null__Ah_Beng()
+
+{
+
+ PersonInfo test = {"","Ah Beng"};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,((void *)0));
+
+ indicator_lastName = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)341, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+
+
+void test_setFirstName_setLastName_given_TanTanTanTanTanTanTanTan__Ah_Beng()
+
+{
+
+ PersonInfo test = {"TanTanTanTanTanTanTanTan","Ah Beng"};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ indicator_lastName = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((0)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)351, UNITY_DISPLAY_STYLE_INT);
+
+}
+
+void test_setFirstName_setLastName_given_Tan__Ah_Beng()
+
+{
+
+ PersonInfo test = {"Tan","Ah Beng"};
+
+
+
+ int indicator,indicator_lastName;
+
+ indicator = setFirstName(&test,&test.firstName);
+
+ indicator_lastName = setLastName(&test,&test.lastName);
+
+ UnityAssertEqualNumber((_U_SINT)((1)), (_U_SINT)(((indicator && indicator_lastName))), (((void *)0)), (_U_UINT)360, UNITY_DISPLAY_STYLE_INT);
 
 }
--- test/test_PersonInfo.c
+++ test/test_PersonInfo.c
@@ -12,7 +12,7 @@ void tearDown(void)
 
 
 /*Testing for age only*/
-void test_setAge_given_negative1_should_return0() 
+void test_setAge_given_negative1_should_return0() //Test age -1 , expect 0, 0 < age < 130
 {
 	PersonInfo test = {.age = -1};
 
@@ -21,7 +21,7 @@ void test_setAge_given_negative1_should_return0()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setAge_given_131_should_return0()
+void test_setAge_given_131_should_return0() //Test age 131, expect 0, larger than 130
 {
 	PersonInfo test = {.age = 131};
 
@@ -30,7 +30,7 @@ void test_setAge_given_131_should_return0()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setAge_given_130_should_return1()
+void test_setAge_given_130_should_return1() //Test age 130, expect 1 , limit at 130
 {
 	PersonInfo test = {.age = 130};
 
@@ -39,7 +39,7 @@ void test_setAge_given_130_should_return1()
 	TEST_ASSERT_EQUAL(1,indicator);
 }
 
-void test_setAge_given_0_should_return1()
+void test_setAge_given_0_should_return1() //Test age 0, expect 0 , age must be greater than 0
 {
 	PersonInfo test = {.age = 0};
 
@@ -48,7 +48,7 @@ void test_setAge_given_0_should_return1()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setAge_given_99_should_return1()
+void test_setAge_given_99_should_return1() //Test age 99 , within age limit
 {
 	PersonInfo test = {.age = 99};
 
@@ -57,8 +57,8 @@ void test_setAge_given_99_should_return1()
 	TEST_ASSERT_EQUAL(1,indicator);
 }
 
-
-void test_setHeight_given_negative_0point01should_return0() 
+/*Testing for height only*/
+void test_setHeight_given_negative_0point01should_return0() //Test height 0.01 , expect 0 ,0 < height < 3
 {
 	PersonInfo test = {.height = -0.01};
 
@@ -67,7 +67,7 @@ void test_setHeight_given_negative_0point01should_return0()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setHeight_given_3point001_should_return0()
+void test_setHeight_given_3point001_should_return0() //Test height 3.001 , expect 0, height larger than 3
 {
 	PersonInfo test = {.height = 3.001};
 
@@ -76,7 +76,7 @@ void test_setHeight_given_3point001_should_return0()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setHeight_given_0_should_return1()
+void test_setHeight_given_0_should_return1() //Test height 0 . expect 0, height must be larger than 0
 {
 	PersonInfo test = {.height = 0};
 
@@ -85,7 +85,7 @@ void test_setHeight_given_0_should_return1()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setHeight_given_3m_should_return1() 
+void test_setHeight_given_3_should_return1() //Test height 3,expect 1 , height at border line 3 meter
 {
 	PersonInfo test = {.height = 3};
 
@@ -94,7 +94,7 @@ void test_setHeight_given_3m_should_return1()
 	TEST_ASSERT_EQUAL(1,indicator);
 }
 
-void test_setHeight_given_1point7_should_return1()
+void test_setHeight_given_1point7_should_return1() //Test height 1.7 , expect 1, height within limits
 {
 	PersonInfo test = {.height = 1.7};
 
@@ -104,7 +104,7 @@ void test_setHeight_given_1point7_should_return1()
 }
 
 
-void test_setTelephone_given_00012345_should_return0()
+void test_setTelephone_given_00012345_should_return0() //Test 00012345, expect 0, telephone number must no start with 0
 {
 	PersonInfo test = {.telephone = 00012345};
 
@@ -113,7 +113,7 @@ void test_setTelephone_given_00012345_should_return0()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setTelephone_given_100000000_should_return0()
+void test_setTelephone_given_100000000_should_return0() //Test 100000000, expect 0,telephone number exceeded 9digits
 {
 	PersonInfo test = {.telephone = 100000000};
 
@@ -122,7 +122,7 @@ void test_setTelephone_given_100000000_should_return0()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setTelephone_given_10023456_should_return1()
+void test_setTelephone_given_10023456_should_return1() //Test 10023456,expect 1 , valid telephone number
 {
 	PersonInfo test = {.telephone = 10023456};
 
@@ -132,8 +132,8 @@ void test_setTelephone_given_10023456_should_return1()
 }
 
  
-
-void test_setFirstName_given_emptystring()
+/*Testing for first names only*/
+void test_setFirstName_given_emptystring() //Test NULL / empty string , expect 0, first name must not be empty
 {
 	PersonInfo test = {.firstName = NULL};
 
@@ -142,7 +142,7 @@ void test_setFirstName_given_emptystring()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setFirstName_given_B3njamin()
+void test_setFirstName_given_B3njamin() //Test B3njamin, expect 0 , first name must not include digits
 {
 	PersonInfo test = {.firstName = "B3njamin"};
 
@@ -151,7 +151,7 @@ void test_setFirstName_given_B3njamin()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setFirstName_given_blank_Benjamin()
+void test_setFirstName_given_blank_Benjamin() //Test (space)Benjamin, expect 0, first name must not start with space
 {
 	PersonInfo test = {.firstName = " Benjamin"};
 
@@ -160,7 +160,7 @@ void test_setFirstName_given_blank_Benjamin()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setFirstName_given_alliance_Benjamin()
+void test_setFirstName_given_alliance_Benjamin() //Test @Benjamin, expect 0, first name contain non alphabet characters
 {
 	PersonInfo test = {.firstName = "@Benjamin"};
 
@@ -169,7 +169,7 @@ void test_setFirstName_given_alliance_Benjamin()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setFirstName_given_Aaron_Anthony_Benjamin_Bernard_27characters_name()
+void test_setFirstName_given_Aaron_Anthony_Benjamin_Bernard_27characters_name() //Test AaronAnthonyBenjaminBernard, expect 0, first name more than 20characters
 {																			    
 	PersonInfo test = {.firstName = "AaronAnthonyBenjaminBernard"};
 
@@ -178,7 +178,7 @@ void test_setFirstName_given_Aaron_Anthony_Benjamin_Bernard_27characters_name()
 	TEST_ASSERT_EQUAL(0,indicator);
 }
 
-void test_setFirstName_given_Benjamin()
+void test_setFirstName_given_Benjamin() //Test Benjamin, expect 1, valid first name
 {
 	PersonInfo test = {.firstName = "Benjamin"};
 
@@ -187,7 +187,7 @@ void test_setFirstName_given_Benjamin()
 	TEST_ASSERT_EQUAL(1,indicator);
 }
 
-void test_setFirstName_given_Benjamin_blank_Ooi()
+void test_setFirstName_given_Benjamin_blank_Ooi() //Test Benjamin Ooi, expect 1, valid first name
 {
 	PersonInfo test = {.firstName = "Benjamin Ooi"};
 
@@ -263,8 +263,8 @@ void test_setLastName_given_Ah_blank_Huat()//Test Ah Huat , expect 1 , valid nam
 
 
 
-
-void test_setFirstName_setLastName_given_Tan__Ah_B3ng()
+/*Testing for first name and last name*/
+void test_setFirstName_setLastName_given_Tan__Ah_B3ng() // Test Tan Ah B3ng , expect 0 , although first name is valid but last name contain digits
 {
 	PersonInfo test = {"Tan","Ah B3ng"};
 
@@ -274,7 +274,7 @@ void test_setFirstName_setLastName_given_Tan__Ah_B3ng()
 	TEST_ASSERT_EQUAL(0,(indicator && indicator_lastName) );
 }
 
-void test_setFirstName_setLastName_given_Tan__Ah_Be_symbol_g()
+void test_setFirstName_setLastName_given_Tan__Ah_Be_symbol_g() //Test Tan Ah Be|\|g, expect 0 , last name contain invalid characters
 {
 	PersonInfo test = {"Tan","Ah Be|\|g"};
 
@@ -284,7 +284,7 @@ void test_setFirstName_setLastName_given_Tan__Ah_Be_symbol_g()
 	TEST_ASSERT_EQUAL(0,(indicator && indicator_lastName) );
 }
 
-void test_setFirstName_setLastName_given_Tan__AhBang_AhBeng_AhBing_Ahh_21_characters_name()
+void test_setFirstName_setLastName_given_Tan__AhBang_AhBeng_AhBing_Ahh_21_characters_name() //Test AhBangAhBengAhBingAhh, expect 0, last name more than 20characters
 {																						   
 	PersonInfo test = {"Tan","AhBangAhBengAhBingAhh"};
 
@@ -294,7 +294,8 @@ void test_setFirstName_setLastName_given_Tan__AhBang_AhBeng_AhBing_Ahh_21_charac
 	TEST_ASSERT_EQUAL(0,(indicator && indicator_lastName) );
 }
 
-void test_setFirstName_setLastName_given_Tan__null()
+
+void test_setFirstName_setLastName_given_Tan__null() //Test Tan NULL, expect 0, last name is empty
 {
 	PersonInfo test = {"Tan",NULL};
 
@@ -304,7 +305,52 @@ void test_setFirstName_setLastName_given_Tan__null()
 	TEST_ASSERT_EQUAL(0,(indicator && indicator_lastName) );
 }
 
-void test_setFirstName_setLastName_given_Tan__Ah_Beng()
+
+
+
+
+
+
+void test_setFirstName_setLastName_given_T_alliance_n__Ah_Beng() //Test T@n Ah Beng, expect 0, first name contain illegal characters
+{
+	PersonInfo test = {"T@n","Ah Beng"};
+
+	int indicator,indicator_lastName;
+	indicator = setFirstName(&test,&test.firstName);
+	indicator_lastName = setLastName(&test,&test.lastName);
+	TEST_ASSERT_EQUAL(0,(indicator && indicator_lastName) );
+}
+
+void test_setFirstName_setLastName_given_T8n__Ah_Beng() //Test T8n Ah Beng, expect 0, first name contain digits
+{
+	PersonInfo test = {"T8n","Ah Beng"};
+
+	int indicator,indicator_lastName;
+	indicator = setFirstName(&test,&test.firstName);
+	indicator_lastName = setLastName(&test,&test.lastName);
+	TEST_ASSERT_EQUAL(0,(indicator && indicator_lastName) );
+}
+
+void test_setFirstName_setLastName_given_null__Ah_Beng() //Test NULL Ah Beng, expect 0, first name is empty
+{
+	PersonInfo test = {"","Ah Beng"};
+
+	int indicator,indicator_lastName;
+	indicator = setFirstName(&test,NULL);
+	indicator_lastName = setLastName(&test,&test.lastName);
+	TEST_ASSERT_EQUAL(0,(indicator && indicator_lastName) );
+}
+
+void test_setFirstName_setLastName_given_TanTanTanTanTanTanTanTan__Ah_Beng() //Test TanTanTanTanTanTanTanTan Ah Beng, expect 0, first name exceeded 20characters
+{
+	PersonInfo test = {"TanTanTanTanTanTanTanTan","Ah Beng"};
+
+	int indicator,indicator_lastName;
+	indicator = setFirstName(&test,&test.firstName);
+	indicator_lastName = setLastName(&test,&test.lastName);
+	TEST_ASSERT_EQUAL(0,(indicator && indicator_lastName) );
+}
+void test_setFirstName_setLastName_given_Tan__Ah_Beng() //Test Tan Ah Beng, expect 1, first name and last name are valid
 {
 	PersonInfo test = {"Tan","Ah Beng"};
 
@@ -313,4 +359,3 @@ void test_setFirstName_setLastName_given_Tan__Ah_Beng()
 	indicator_lastName = setLastName(&test,&test.lastName);
 	TEST_ASSERT_EQUAL(1,(indicator && indicator_lastName) );
 }
-
